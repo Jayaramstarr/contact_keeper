da6ee9b2800509237cf6dbcf3af4261b09112261
unknown <jayaramstarr33@gmail.com>
2021-01-05 23:08:12 +0530

    Initial Commit

---
+++ client/src/components/contacts/ContactFilter.js
@@ -0,0 +1,30 @@
+import React, { useContext, useRef, useEffect } from 'react'
+import ContactContext from '../../context/contact/contactContext'
+
+const ContactFilter = () => {
+    
+    const contactContext = useContext(ContactContext)
+    const { filtered, filterContacts, clearFilter } = contactContext
+    
+    useEffect( () => {
+        if( filtered === null )
+            text.current.value = ''
+    })
+
+    const text = useRef('')
+
+    const onChange = e => {
+        if( text.current.value !== '' )
+            filterContacts(e.target.value)
+        else
+            clearFilter()
+    }
+
+    return (
+        <form>
+            <input type="text" ref={text} placeholder="Filter Contacts..." onChange={onChange}/>
+        </form>
+    )
+}
+
+export default ContactFilter
---
+++ routes/contacts.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const router = express.Router()
+const auth = require('../middleware/auth')
+const { check, validationResult } = require('express-validator')
+
+const User = require('../models/User')
+const Contact = require('../models/Contact')
+const { findByIdAndDelete } = require('../models/User')
+
+
+// @route   GET api/contacts
+// @desc    Get all user contacts
+// @access  Private
+router.get('/', auth, async (req, res) => { 
+    try {
+        const contacts = await Contact.find({ user:req.user.id }).sort({ date: -1 })
+        res.json(contacts)
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route   POST api/contacts
+// @desc    Add new contacts
+// @access  Private
+router.post('/', 
+    [auth, [
+        check('name', 'Name is required')
+            .not()
+            .isEmpty()
+    ]], 
+    async (req, res) => { 
+    const errors = validationResult(req)
+    if(!errors.isEmpty())
+        return res.status(400).json({ errors: errors.array() })
+    
+    const { name, phone, email, type } = req.body
+
+    try {
+        const newContact = new Contact({
+            name, email, phone, type, user:req.user.id
+        })
+
+        const contact = await newContact.save()
+        res.json(contact)
+    } catch (err) {
+        console.log(err)
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route   PUT api/contacts/:id
+// @desc    Update contact
+// @access  Private
+router.put('/:id', auth, async (req, res) => { 
+    const { name, phone, email, type } = req.body
+
+    const contactFeilds = {}
+    if(name) contactFeilds.name = name
+    if(email) contactFeilds.email = email
+    if(phone) contactFeilds.phone = phone
+    if(type) contactFeilds.type = type
+
+    try {
+        let contact = await Contact.findById(req.params.id)
+        if(!contact) return res.status(404).json({ msg: 'Contact not found' })
+        if(contact.user.toString() !== req.user.id)
+            return res.status(401).json({ msg: 'Not authorized' })
+        contact = await Contact.findByIdAndUpdate(req.params.id,
+            { $set: contactFeilds},
+            { new: true })
+        res.json(contact)
+    } catch (err) {
+        console.log(err)
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route   DELETE api/contacts/:id
+// @desc    Delete contact
+// @access  Private
+router.delete('/:id', auth, async (req, res) => { 
+    try {
+        let contact = await Contact.findById(req.params.id)
+        if(!contact) return res.status(404).json({ msg: 'Contact not found' })
+        if(contact.user.toString() !== req.user.id)
+            return res.status(401).json({ msg: 'Not authorized' })
+        await Contact.findByIdAndRemove(req.params.id)
+        res.json({ msg: 'Contact removed' })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send('Server Error')
+    }
+})
+
+module.exports = router
